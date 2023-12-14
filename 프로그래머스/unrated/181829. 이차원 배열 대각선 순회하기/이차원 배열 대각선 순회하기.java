269d0a9562b4e343a4371c5b685c0edabef4dc96
김정수 <133090170+LooklikeDinosour@users.noreply.github.com>
2023-12-15 01:20:07 +0900

    [level unrated] Title: 이차원 배열 대각선 순회하기, Time: 0.04 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.java
+++ 프로그래머스/unrated/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.java
@@ -2,12 +2,12 @@ class Solution {
     public int solution(int[][] board, int k) {
         int answer = 0;
         for(int i = 0; i < board.length; i++) {
+            if(i > k) {
+                break;
+            }
             for(int j = 0; j < board[i].length; j++) {
-                if(i+j>k) {
-                    continue;
-                } else {
+                if(j +i <= k)
                     answer += board[i][j];
-                }
             }
         }
         return answer;
