2d92a4ce4a449df5495448caaad0bcb4ccc4ca10
김정수 <133090170+LooklikeDinosour@users.noreply.github.com>
2023-12-17 00:27:40 +0900

    [level 1] Title: 폰켓몬, Time: 2.61 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int numLength = nums.length;
+        int pickMax = nums.length/2; //최대갯수
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < numLength; i++) {
+            set.add(nums[i]);
+        }
+        answer = set.size();
+        if(answer > pickMax) {
+            return pickMax;
+        } else {
+            return answer;
+        }
+    }
+}
