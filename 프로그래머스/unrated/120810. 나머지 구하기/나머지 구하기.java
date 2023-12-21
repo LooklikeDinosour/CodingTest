b6dec803e9284cd19bd3107d2bb20598a27893f1
김정수 <133090170+LooklikeDinosour@users.noreply.github.com>
2023-12-22 00:32:31 +0900

    [level unrated] Title: 나머지 구하기, Time: 0.01 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120810. 나머지 구하기/나머지 구하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        if(num1<=100 && 0<num1 && 0<num2 && num2 <=100) {
+            return num1%num2;
+        } 
+        return answer;
+    }
+}
