3797df2d558ea6b367d5277981989a5cc46934e9
김정수 <133090170+LooklikeDinosour@users.noreply.github.com>
2024-04-29 18:27:03 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.36 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,8 +1,10 @@
 class Solution {
     public String[] solution(String my_string) {
         String[] answer = {};
-        my_string = my_string.trim().replaceAll("[\\s]{1,}", " ");
-        answer = my_string.split(" ");
+        answer = my_string
+            .trim()
+            .replaceAll("[\\s]{2,}", " ")
+            .split(" ");
         return answer;
     }
 }
